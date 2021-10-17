dfeb269ba8bb26de7ad178fa23fdeb8d67632402
Eduardo Santana <ezambomsantana@gmail.com>
2021-10-16 23:41:57 -0300

    continua admin

--- kafka-admin/src/main/java/com/santana/kafka/admin/KafkaAdmin.java
+++ kafka-admin/src/main/java/com/santana/kafka/admin/KafkaAdmin.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.DeleteTopicsResult;
 import org.apache.kafka.clients.admin.DescribeTopicsResult;
 import org.apache.kafka.clients.admin.ListTopicsResult;
 import org.apache.kafka.clients.admin.NewTopic;
@@ -19,16 +20,22 @@ public class KafkaAdmin {
 		topics.names().get().forEach(System.out::println);
 	}
 	
-	public static void createTopic(String topicName, int partitions, short replications, AdminClient adminClient) {
+	public static void createTopic(String topicName, 
+			int partitions, 
+			short replications, 
+			AdminClient adminClient) {
 
-		final NewTopic newTopic = new NewTopic(topicName, partitions, replications);
+		final NewTopic newTopic = 
+				new NewTopic(topicName, partitions, replications);
 		List<NewTopic> topics = new ArrayList<NewTopic>();
 		topics.add(newTopic);
 		try {
-			final CreateTopicsResult result = adminClient.createTopics(topics);
+			CreateTopicsResult result = 
+					adminClient.createTopics(topics);
 			result.all().get();
 		} catch (final Exception e) {
-			throw new RuntimeException("Failed to create topic:" + topicName, e);
+			throw new RuntimeException("Failed to create topic:" 
+					+ topicName, e);
 		}
 
 	}
@@ -36,12 +43,26 @@ public class KafkaAdmin {
 	public static void describeTopic(String topicName, AdminClient adminClient) 
 			throws InterruptedException, ExecutionException {
 			
-		List<String> topicNames = new ArrayList();
+		List<String> topicNames = new ArrayList<>();
 		topicNames.add(topicName);
 		
 		DescribeTopicsResult topics = adminClient.describeTopics(topicNames);
-		topics.all().get().forEach((x, y) -> System.out.println(x + " " + y.topicId() + " " + y.partitions()));
+		topics.all().get().forEach(
+				(x, y) -> System.out.println(x + " " + y.topicId() + " " + y.partitions()));
 	}
 	
 
+	public static void deleteTopic(String topicName, AdminClient adminClient)  {
+			
+		List<String> topicNames = new ArrayList<>();
+		topicNames.add(topicName);
+		try {
+			DeleteTopicsResult topics = adminClient.deleteTopics(topicNames);
+			topics.all().get();
+		} catch (final Exception e) {
+			throw new RuntimeException("Failed to delete topic:" 
+					+ topicName, e);
+		}
+	}
+	
 }
--- kafka-admin/src/main/java/com/santana/kafka/admin/Main.java
+++ kafka-admin/src/main/java/com/santana/kafka/admin/Main.java
@@ -8,20 +8,30 @@ import org.apache.kafka.clients.admin.AdminClientConfig;
 
 public class Main {
 
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
+	public static void main(String[] args) 
+			throws InterruptedException, ExecutionException {
 		
 		Properties properties = new Properties();
-		properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.put(
+				AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, 
+				"localhost:9092");
 		
 		AdminClient adminClient = AdminClient.create(properties);
 		
-	//	KafkaAdmin.createTopic("topico-4", 2, (short) 1, adminClient);
-	//	KafkaAdmin.createTopic("topico-5", 2, (short) 1, adminClient);
-		
-		
+		//KafkaAdmin.createTopic("topico-4", 
+		//		2, (short) 1, adminClient);
+	//	KafkaAdmin.createTopic("topico-5", 
+		//		2, (short) 1, adminClient);
+				
 		KafkaAdmin.listTopic(adminClient);
 		
 		KafkaAdmin.describeTopic("topico-1", adminClient);
+		
+
+		KafkaAdmin.deleteTopic("topico-5", adminClient);
+				
+		KafkaAdmin.listTopic(adminClient);
+				
 
 	}
 
