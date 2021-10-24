60a6c7ca751906a64cb674cf347a1741685f2dd3
Eduardo Santana <ezambomsantana@gmail.com>
2021-10-24 11:25:22 -0300

    python app

---
+++ python-consumer/src/consumer.py
@@ -0,0 +1,37 @@
+import threading
+import json
+import logging
+from kafka import KafkaConsumer
+
+class Consumer(threading.Thread):
+            
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.stop_event = threading.Event()
+
+    def run(self):
+        try:
+            consumer = KafkaConsumer(
+                bootstrap_servers=['localhost:9092'],
+                group_id="grupo_python",                         
+                consumer_timeout_ms=30000
+            )
+            consumer.subscribe(['SHOP_TOPIC_EVENT'])
+
+            while not self.stop_event.is_set():
+                for message in consumer:
+                    logging.info(
+                        'Receiving message from topic SHOP_TOPIC_EVENT')
+                    self.messages.append(
+                        json.loads(message.value.decode("utf-8")))
+                    
+
+            consumer.close()      
+        except IOError as exception:
+            print("Read error")
+            logging.error(exception)
+
+    def get_messages():
+        return messages
+
+    messages = []
--- python-consumer/src/main.py
+++ python-consumer/src/main.py
@@ -1,10 +1,33 @@
 from flask import Flask
+from consumer import Consumer
+import json
+import logging
+
+# inicializa o consumidor
+consumer = Consumer()
+consumer.start()
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def get_messages():
+    messages = consumer.get_messages()
+    return json.dumps(messages)
+
+@app.route('/last')
+def get_messages():
+    messages = consumer.get_messages()
+    return json.dumps(messages)
+
+@app.route('/last_ten')
+def get_messages():
+    messages = consumer.get_messages()
+    return json.dumps(messages)
+
+@app.route('/stop_consumer')
+def stop_consumer():
+    consumer.stop()
+    return 'consumer stopped'
 
 if __name__ == '__main__':
      app.run()
